0931bc78643739fbb8df82094815b28af87fd291
Chandrima Ghosh <chandrima0141@gmail.com>
2023-10-16 17:40:05 +0530

    Csharp

---
+++ C#/ifelseOperator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ifelseOperator
+{
+    internal class Operator
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, ans, choice;
+
+            Console.WriteLine("Enter num1 :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter num2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Exit");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+
+            if(choice == 1 )
+            {
+                ans = num1 + num2;
+                Console.WriteLine("Addition : " + ans);
+            }
+            else if(choice == 2)
+            {
+                ans = num1 - num2;
+                Console.WriteLine("Substration : " + ans);
+            }
+            else if(choice == 3)
+            {
+                ans = num1 * num2;
+                Console.WriteLine("Multiplication : " + ans);
+            }
+            else if(choice == 4)
+            {
+                ans = num1 / num2;
+                Console.WriteLine("Division : " + ans);
+            }
+            else if(choice == 5)
+            {
+                Console.WriteLine("Exit");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ c programming/exam1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n=0;
+	int m=0;
+	if (n>0 && m>0)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+	return 0;
+}
