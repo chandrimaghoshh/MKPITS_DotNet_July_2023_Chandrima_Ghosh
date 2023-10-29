a88fe8ade5754bb03bfe09f616c3a05498e39e19
Chandrima Ghosh <chandrima0141@gmail.com>
2023-10-29 21:47:57 +0530

    Csharp

---
+++ C#/Array_2_Largest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Array_2_largest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j = 0, lrg, lrg2nd;
+            int[] arr1 = new int[3];
+
+            Console.Write("Input the size of array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < 3; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            lrg = 0;
+
+            for (i = 0; i < 3; i++)
+            {
+                if (lrg < arr1[i])
+                {
+                    lrg = arr1[i];
+                    j = i;
+                }
+            }
+            
+            lrg2nd = 0;
+            for (i = 0; i < 3; i++)
+            {
+                if (i == j)
+                {
+                    i++; 
+                    i--;
+                }
+                else
+                {
+                    if (lrg2nd < arr1[i])
+                    {
+                        lrg2nd = arr1[i];
+                    }
+                }
+            }
+
+            Console.Write("The Second largest element in the array is :  {0} \n\n", lrg2nd);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Array_2_Smallest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Array_2_Smallest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j = 0, sml, sml2nd;
+            int[] arr1 = new int[3];
+
+            Console.Write("Input the size of array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Input {0} elements in the array (value must be <9999):\n", n);
+            for (i = 0; i < 3; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+       
+            sml = 0;
+            for (i = 0; i < 3; i++)
+            {
+                if (sml > arr1[i])
+                {
+                    sml = arr1[i];
+                    j = i;
+                }
+            }
+
+ 
+            sml2nd = 99999;
+            for (i = 0; i < 3; i++)
+            {
+                if (i == j)
+                {
+                    i++;
+                    i--;
+                }
+                else
+                {
+                    if (sml2nd > arr1[i])
+                    {
+                        sml2nd = arr1[i];
+                    }
+                }
+            }
+
+            Console.Write("The Second smallest element in the array is :  {0} \n\n", sml2nd);
+
+            Console.ReadKey();
+        }
+    }
+}
