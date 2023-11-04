bea1049ebbf4f4f11b6dfe2f8dc64355e5a24e68
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-04 21:41:44 +0530

    Csharp

---
+++ C#/Oops/array_print_3of3_matrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPrint3of3Matri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    arr[row, col] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(arr[row, col] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/class_Emp_HighSal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_Emp_HighSal
+{
+    internal class Program
+    {
+        class Employee
+        {
+            string name;
+            int id;
+            int salary;
+
+            public void getdata(string name, int id, int salary)
+            {
+                this.name = name;
+                this.id = id;
+                this.salary = salary;
+            }
+            public void displaydata()
+            {
+                Console.WriteLine("Employee Name : " + name);
+                Console.WriteLine("Employee Id : " + id);
+                Console.WriteLine("Employee Salary : " + salary);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee[]emp = new Employee[3];
+            emp[0] = new Employee();
+            emp[1] = new Employee();
+            emp[2] = new Employee();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter Emplyoee Name : ");
+                string en = Console.ReadLine();
+
+                Console.WriteLine("Enter Emplyoee Id : ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Emplyoee Salary : ");
+                int sal = Convert.ToInt32(Console.ReadLine());
+
+                emp[i].getdata(en, id, sal);
+            }
+                
+            for(int i=0; i < 3; i++)
+            {
+                emp[i].displaydata();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
