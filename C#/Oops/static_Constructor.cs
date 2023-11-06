94c0d4d37394699dd5c6b8fcb333338ea15c0087
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-06 22:51:38 +0530

    Csharp

---
+++ C#/Oops/static_Constructor.cs
@@ -0,0 +1,28 @@
+﻿using staticConstractor;
+using System;
+namespace staticConstractor
+{
+    public class employee
+    {
+        static employee() // Static constructor
+        { 
+            Console.WriteLine("The static constructor ");
+
+        }
+        public static void Salary()
+        {
+            Console.WriteLine();
+            Console.WriteLine("The Salary method");
+        }
+    }
+}
+class details
+{
+    static void Main()
+    {
+        Console.WriteLine("----------Static constrctor example by chester------------------");
+        Console.WriteLine();
+        employee.Salary();
+        Console.ReadLine();
+    }
+}
