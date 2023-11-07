d9e89d24161a59e68053526054b501dfbcac59d0
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-07 17:01:39 +0530

    Csharp

---
+++ C#/Oops/method_Overloading_Addition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_Overloading_Addition
+{
+    class calculate
+    {
+        public void addition(int n1, int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("Addition of integer num : " + res);
+        }
+        public void addition(float n1, float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("Additon of float num : " + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate cal = new calculate();
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Calling Addition of Integer Number : ");
+            cal.addition(2, 3);
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Now Calling Addition of Float Number :");
+            cal.addition(2.0f, 4.0f);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/destructor_Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace destructor_Employee
+{
+    class Employee
+    {
+        //creating a constructor
+
+        public Employee()
+        {
+            Console.WriteLine("Constructor called");
+        }
+        ~Employee()
+        {
+            Console.WriteLine("Destructor called");
+        }
+    }
+                    //use clt F5 for full output and stay the output
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            Console.ReadKey();
+
+            Employee e2 = new Employee();
+            Console.ReadKey();
+        }
+    }
+}
