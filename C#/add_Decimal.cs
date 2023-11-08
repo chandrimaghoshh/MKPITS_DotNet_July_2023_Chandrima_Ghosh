e8c4f0d73db2050ab3edb56ce82e8e97c4eb90a2
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-08 23:49:55 +0530

    Csharp

---
+++ C#/add_Decimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n1, n2, result = 0;
+            Console.WriteLine("num1:");
+            n1=Convert
+        }
+    }
+}
