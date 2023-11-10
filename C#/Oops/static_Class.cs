49cd42f754f4d0d824515d712aae69de8e6d5d1b
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-10 22:41:38 +0530

    Csharp

---
+++ C#/Oops/static_Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_Class
+{
+    static class Math
+    {
+        public static float pi = 3.14f;
+        public static int cube(int n)
+        {
+            return n * n * n;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value of Pi : " + Math.pi);
+            Console.WriteLine("Value of cube : " + Math.cube(5));
+
+            Console.ReadKey();
+        }
+    }
+}
