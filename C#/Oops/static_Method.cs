1a5ad237c498d31b1dac551d5ab503f225a61d45
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-11 22:15:19 +0530

    Csharp

---
+++ C#/Oops/static_Method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_Method
+{
+    internal class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("Hello from static method");
+        }
+        static void Main(string[] args)
+        {
+            display();
+
+            Console.ReadKey();  //to access static method display instance is not required
+        }
+    }
+}
