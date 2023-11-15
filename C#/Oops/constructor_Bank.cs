25cb67c598716b98a38521b7ab1747fccf6c919d
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-15 19:39:37 +0530

    Csharp

---
+++ C#/Oops/constructor_Bank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_Bank
+{
+    class Bank
+    {
+        int act_no;
+        string name;
+        int deposit;
+        int withdrawal;
+        int bal, amt;
+
+        public Bank()
+        {
+            act_no = 123;
+            name = "Chandrima";
+            bal = 1000;
+            amt = 500;
+
+            deposit = bal + amt;
+            withdrawal = bal - amt;
+        }
+        public void display()
+        {
+            Console.WriteLine("Deposit Amount = " + deposit);
+            Console.WriteLine("Withdrawal Amount = " + withdrawal);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank();
+
+            b.display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/constructor_Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace constructor_Circle
+{
+    class Circle
+    {
+        int radius;
+        float circle;
+        float circ;
+
+        public Circle()
+        {
+            radius = 5;
+            circle = 3.14f * radius * radius;
+            circ = 2 * 3.14f * radius;
+        }
+        public void display()
+        {
+            Console.WriteLine("Area of Circle = " + circle);
+            Console.WriteLine("Area of Circumference = " + circ);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.display();
+
+            Console.ReadKey();
+        }
+    }
+}
