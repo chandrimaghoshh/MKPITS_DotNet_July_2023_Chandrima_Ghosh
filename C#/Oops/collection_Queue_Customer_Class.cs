eef2bee315298e21fc2c880f8858658b8f17d50a
Chandrima Ghosh <chandrima0141@gmail.com>
2023-11-24 22:26:11 +0530

    Csharp

---
+++ C#/Oops/collection_Queue_Customer_Class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection_Queue_Customer
+{
+    class Customer
+    {
+        public int customerId;
+        public string customerName;
+        public string customerEmail;
+        public int customerMobileNo;
+
+        public Customer(int customerId, string customerName, string customerEmail, int customerMobileNo)
+        {
+            this.customerId = customerId;
+            this.customerName = customerName;
+            this.customerEmail = customerEmail;
+            this.customerMobileNo = customerMobileNo;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer(1, "Chandrima", "chandrima@1410", 90965);
+            Customer c2 = new Customer(2, "Akash", "akash@26", 90950);
+            Customer c3 = new Customer(3, "Pooja", "pooja@09", 80976);
+
+            Queue c = new Queue();
+            c.Enqueue(c1);
+            c.Enqueue(c2);
+            c.Enqueue(c3);
+
+            foreach (Customer s in c)
+            {
+                Console.WriteLine("Customer Id : " + s.customerId);
+                Console.WriteLine("Customer Name : " + s.customerName);
+                Console.WriteLine("Customer Email : " + s.customerEmail);
+                Console.WriteLine("Customer Moblie No. : " + s.customerMobileNo);
+                Console.WriteLine("------------------------------------------------------------");
+            }
+            Customer r = (Customer)c.Dequeue();
+            Console.WriteLine("Item dequeue : " + r.customerId + " : " + r.customerName + " : " + r.customerEmail + " : " + r.customerMobileNo);
+
+            Console.WriteLine("After Dequeue : ");
+            foreach (Customer s in c)
+            {
+                Console.WriteLine("Name : " + s.customerName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/collection_Queue_Student_class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace collection_Quaue_Student_class
+{
+    class Student
+    {
+        public int rno;
+        public string name;
+        public Student(int rno, string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(10, "Chandrima");
+            Student s2 = new Student(11, "Pooja");
+            Student s3 = new Student(12, "Akash");
+
+            Queue s = new Queue();
+            s.Enqueue(s1);
+            s.Enqueue(s2);
+            s.Enqueue(s3);
+            foreach (Student val in s)
+            {
+                Console.WriteLine("Roll No. : " + val.rno);
+                Console.WriteLine("Name : " + val.name);
+            }
+            Student r = (Student)s.Dequeue();
+            Console.WriteLine("item deque : " + r.rno + " : " + r.name);
+            Console.WriteLine("after deque : ");
+            foreach (Student val in s)
+            {
+                Console.WriteLine("Name " + val.name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
