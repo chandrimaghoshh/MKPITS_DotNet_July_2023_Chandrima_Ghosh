882973697835b6cbc290e1847d4248a2d3237d5c
Chandrima Ghosh <chandrima0141@gmail.com>
2023-12-03 19:00:20 +0530

    Csharp

---
+++ C#/WebForm/Form_Dayno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Dayno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dayno d = new Dayno();
+            string res = d.day(Convert.ToInt32(textBox1.Text));
+            label2.Text = res;
+        }
+    }
+}
---
+++ C#/WebForm/Form_Vowel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Vowel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vowel v = new Vowel();
+            string res = v.vowel(Convert.ToChar(textBox1.Text));
+            label2.Text = res;
+        }
+    }
+}
