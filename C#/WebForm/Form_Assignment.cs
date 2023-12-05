ef690fab8e35c95e88d9ca967dd1701c19365259
Chandrima Ghosh <chandrima0141@gmail.com>
2023-12-05 23:11:39 +0530

    Csharp

---
+++ C#/WebForm/Form_Assignment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            numericUpDown1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Enter name");
+                MessageBox.Show("Enter age");
+            }
+            else
+            {
+               
+            }
+
+            if(numericUpDown1.Value<=18)
+            {
+                MessageBox.Show("Please enter age greater than 18");
+            }
+            else
+            {
+                sb.Append("Name : " + textBox1.Text + "\n");
+                sb.Append("Age : " + numericUpDown1.Value.ToString());
+            }
+            label3.Text = sb.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome");
+        }
+    }
+}
---
+++ C#/WebForm/Form_Customer_Product.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Customer__product
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome To Our Shop");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if(textBox1.Text=="" && textBox2.Text=="" && textBox3.Text=="")
+            {
+                MessageBox.Show("Enter Details first");
+            }
+            else
+            {
+                sb.Append("Customer Name : " + textBox1.Text + "\n");
+                sb.Append("Mobile No. : " + maskedTextBox1.Text + "\n");
+                sb.Append("Product catogory : " + comboBox1.Text + "\n");
+                sb.Append("Product Item : " + comboBox2.Text + "\n");
+                sb.Append("Product Rate : " + textBox2.Text + "\n");
+                sb.Append("Product Quantity : " + textBox3.Text + "\n");
+
+                int rate = Convert.ToInt32(textBox2.Text);
+                int quantity= Convert.ToInt32(textBox3.Text);
+                int total = rate * quantity;
+                sb.Append("Total = " + total + "\n");
+            }
+
+            label8.Text=sb.ToString();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            label8.Text = maskedTextBox1.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox2.Text = " ";
+            switch(comboBox1.Text)
+            {
+                case "Electronics":
+                    comboBox2.Items.Add("Laptop");
+                    comboBox2.Items.Add("Tv");
+                    comboBox2.Items.Add("Microwave");
+                    comboBox2.Items.Add("Fridge");
+                    comboBox2.Items.Add("Washing Machine");
+                    comboBox2.Items.Add("Ac");
+                    break;
+                case "Furniture":
+                    comboBox2.Items.Add("Chair");
+                    comboBox2.Items.Add("Sofa");
+                    comboBox2.Items.Add("Bed");
+                    comboBox2.Items.Add("Sofa-cum bed");
+                    comboBox2.Items.Add("Tabel");
+                    comboBox2.Items.Add("Cupboard");
+                    break;
+                case "Household":
+                    comboBox2.Items.Add("Curtains");
+                    comboBox2.Items.Add("Utensils");
+                    comboBox2.Items.Add("Bedsheet");
+                    comboBox2.Items.Add("Flower vase");
+                    comboBox2.Items.Add("Mat");
+                    break;
+                case "Sports":
+                    comboBox2.Items.Add("Cricket products");
+                    comboBox2.Items.Add("Football products");
+                    comboBox2.Items.Add("Voleyball products");
+                    comboBox2.Items.Add("Basketball products");
+                    comboBox2.Items.Add("Badminton products");
+                    break;
+            }
+            
+        }
+    }
+}
