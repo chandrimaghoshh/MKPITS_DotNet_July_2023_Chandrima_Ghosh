d71c60f6e4c634f0846fa5cd6388d1b206a61be4
Chandrima Ghosh <chandrima0141@gmail.com>
2023-12-08 22:16:02 +0530

    Csharp

---
+++ C#/WebForm/Form1 - Copy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Mdi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void registrationFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Registration_Form r = new Registration_Form();
+            r.MdiParent = this;
+            r.Show();
+        }
+
+        private void accountFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Account_Form a = new Account_Form();
+            a.MdiParent = this;
+            a.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Registration_Form r = new Registration_Form();
+            r.MdiParent = this;
+            r.Show();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Account_Form a = new Account_Form();
+            a.MdiParent = this;
+            a.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "admin") 
+            {
+                panel1.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Invaild input");
+            }
+        }
+    }
+}
---
+++ C#/WebForm/Registration_Form - Copy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Mdi
+{
+    public partial class Registration_Form : Form
+    {
+        public Registration_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name :" + textBox1.Text + "\n");
+            sb.Append("Email :" + textBox2.Text + "\n");
+            sb.Append("DOB :");
+        }
+    }
+}
