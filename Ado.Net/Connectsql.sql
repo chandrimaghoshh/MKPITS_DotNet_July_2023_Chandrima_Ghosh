dac3a73d12f64ca93dc33092218a10fcd2aee08b
Chandrima Ghosh <chandrima0141@gmail.com>
2024-01-02 22:02:16 +0530

    Ado.Net

---
+++ Ado.Net/Connectsql.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [UserId]
+      ,[UserName]
+      ,[Password]
+  FROM [MkpSample].[dbo].[dbo.User]
---
+++ Ado.Net/UserInfo.sql
@@ -0,0 +1,7 @@
+create table userdetails(Username varchar(20),Password nvarchar(20))
+
+insert into userdetails values('ChandrimaGhosh','cg124')
+insert into userdetails values('AkashGautam','ag133')
+insert into userdetails values('PoojaTambe','pt201')
+
+select * from userdetails
