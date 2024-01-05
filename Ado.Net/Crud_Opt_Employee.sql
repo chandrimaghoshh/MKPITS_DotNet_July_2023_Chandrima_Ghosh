38843f5d8292724f91c9e0c20dcabf17d21d9e84
Chandrima Ghosh <chandrima0141@gmail.com>
2024-01-05 16:14:26 +0530

    Ado.Net

---
+++ Ado.Net/Crud_Opt_Employee.sql
@@ -0,0 +1,8 @@
+create table employee(Id int primary key, Name varchar(20), DOJ varchar(20),
+Age int, City varchar(20), Mobile varchar(20), Salary int)
+
+insert into employee values(100,'Chandrima','2023/12/12',21,'Nagpur','9096569768',60000)
+insert into employee values(102,'Pooja','2023/12/15',31,'Amravati','9095264390',50000)
+insert into employee values(103,'Kareena','2023/12/15',21,'Akola','8094269760',50000)
+
+select * from employee
